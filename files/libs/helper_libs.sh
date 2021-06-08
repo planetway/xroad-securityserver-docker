2c58dab53e52837e7a1f84bb3e197aadd2eb9d99
Kristjan Kullerkann <kristjan.kullerkann@planetway.com>
2021-06-08 14:45:48 +0300

    refactor helper_libs.sh to pass shellcheck

--- files/libs/helper_libs.sh
+++ files/libs/helper_libs.sh
@@ -8,12 +8,11 @@
 #
 # functions
 log() {
-  pwd=$(pwd)
-  echo -e "$(date "+%Y-%m-%d %H:%M:%S,%3N") $@" >&2
+  echo -e "$(date "+%Y-%m-%d %H:%M:%S,%3N") $*" >&2
 }
 
 check() {
-  if test $1 -ne 0
+  if test "$1" -ne 0
   then
     log "$2"
     exit 1
@@ -28,7 +27,7 @@ die () {
     rc=$1
     shift
     warn "$@"
-    exit $rc
+    exit "$rc"
 }
 
 check_postgres () {
@@ -36,7 +35,7 @@ check_postgres () {
     local checkdb_user=${PX_CHECKDB_USER:-$POSTGRES_USER}
     local checkdb_user=${checkdb_user:-postgres}
     local checkdb_password=${PX_CHECKDB_PASSWORD:-$POSTGRES_PASSWORD}
-    until PGPASSWORD=$checkdb_password psql -h $POSTGRES_HOST -p $POSTGRES_PORT -U $checkdb_user -d postgres -c '\q' > /dev/null 2>&1; do
+    until PGPASSWORD=$checkdb_password psql -h "$POSTGRES_HOST" -p "$POSTGRES_PORT" -U "$checkdb_user" -d postgres -c '\q' > /dev/null 2>&1; do
         >&2 log "Postgres is unavailable - waiting"
         sleep 1
     done
